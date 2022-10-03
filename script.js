2ae00eb557b7724ef35600c8778f9e0eb4ab7e0c
ChristiaanDD <93121676+ChristiaanDD@users.noreply.github.com>
2022-10-03 09:36:06 +0000

    balletje dat stuitert met spatie

--- script.js
+++ script.js
@@ -1,21 +1,53 @@
-let y = 0;
-let speed=1;
-let gravity=0.2;
-
-function setup() { 
-  createCanvas(400, 400);
-} 
-
-function draw(){ 
-  background(220);
-  ellipse(30,y,20,20);
-  y=y+speed;
-  speed=speed+gravity;
-  
-  if(y>400){
-  //reverse the speed
-    speed=-0.95 * speed;
+class Birb {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.vy = 0;
+    this.gravity = 0.2;
   }
-  
 
+  drawBirb() {
+    fill("red");
+    circle(this.x, this.y, 10)
+
+    this.vy += this.gravity;
+
+    this.y += this.vy;
+
+    if (this.y > 380) {
+      this.vy = 0;
+      this.y = 380;
+    }
+
+    if (this.y < 0) {
+      this.vy = 0;
+      this.y = 0;
+    }
+  }
+}
+
+
+var birb;
+
+function setup() {
+  createCanvas(500, 400);
+
+  birb = new Birb(100, 200);
+
+}
+
+
+function draw() {
+  background(225);
+
+
+
+  birb.drawBirb();
+
+}
+
+function keyPressed() {
+  if (keyCode == 32) {
+    birb.vy -= 5;
+  }
 }
