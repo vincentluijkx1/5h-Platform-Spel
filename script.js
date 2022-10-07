8fe0cdccd7f90bab0828aab6a99ca30f98bf11ce
Vincent Luijkx <93121307+vincentluijkx1@users.noreply.github.com>
2022-10-07 08:39:50 +0000

    Balletje kan opzij

--- script.js
+++ script.js
@@ -25,39 +25,40 @@ class Birb {
       this.vy = 0;
       this.y = 0;
     }
+
+    if (keyIsDown(LEFT_ARROW)) {
+      this.x -= 5;
+    }
+
+    if (keyIsDown(RIGHT_ARROW)) {
+      this.x += 5;
+    }
   }
 }
 
+
 var birb;
-var blocks = [];
 
 function setup() {
   createCanvas(500, 400);
 
-  birb = new Birb(100, 200);  
+  birb = new Birb(100, 200);
 
 }
 
 
 function draw() {
   background(225);
-
-  // elke 50 frames iets doen
-  if(frameCount % 50 == 0){
-    blocks.push(new Block());
-  }
-
-  blocks.forEach(b => {
-    b.draw();
-  });
-  
-  
+  fill(0);
+  rect(75, 40, 60, 10);
   birb.drawBirb();
 
 }
 
 function keyPressed() {
   if (keyCode == 32) {
-    birb.vy -= 5;
+      birb.vy -= 5;
+    }
   }
-}
+
+
